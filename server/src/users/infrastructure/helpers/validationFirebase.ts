c630da1428386a26a204cabb64cec87cc68945c9
MateoGuerreroE <mateof1223@gmail.com>
2023-11-09 18:37:48 -0500

    Deploying

--- server/src/users/infrastructure/helpers/validationFirebase.ts
+++
@@ -1,6 +0,0 @@
-export default validationFirebase = (error: any) => {
-  if (error.code === 'auth/email-already-in-use') throw new Error('Email already in use')
-  if (error.code === 'auth/invalid-email') throw new Error('Invalid email')
-  if (error.code === 'auth/invalid-password') throw new Error('Invalid password')
-  if (error.code === 'auth/invalid-password') throw new Error('Invalid password')
-}
