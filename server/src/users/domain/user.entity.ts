afb72ef9d6edd468b33a468c046a548d3849c7ab
MateoGuerreroE <mateof1223@gmail.com>
2023-11-09 17:10:02 -0500

    Phone and country added to user

--- server/src/users/domain/user.entity.ts
+++ server/src/users/domain/user.entity.ts
@@ -3,6 +3,8 @@ import { type Types } from 'mongoose'
 export interface UserEntity {
   _id?: Types.ObjectId | null
   name: string
+  country: string
+  phone: string
   email: string
   password?: string | null //* Important to decide If we keep It, otherwise we need to create another interface */
   role?: string | null
