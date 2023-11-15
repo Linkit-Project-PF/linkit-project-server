c93cbcf46a8135f296c0962259ad97f1f4c501d2
Juan Velez <juanvelez.dev@gmail.com>
2023-11-15 11:25:40 -0500

    Update Access-Control-Allow headers and methods in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,14 +15,14 @@ app.use(cors(
 ))
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
 app.use(express.json())
 
