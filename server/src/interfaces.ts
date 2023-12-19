3426c9dc17e94416dea10e03d69b22cc22b0a122
MateoGuerreroE <mateof1223@gmail.com>
2023-12-19 16:47:14 -0500

    Set TODOs for postulation entities

--- server/src/interfaces.ts
+++ server/src/interfaces.ts
@@ -2,3 +2,5 @@ export interface translatedResponse {
   en: string
   es: string
 }
+
+// TODO Create postulation interface here to define find postulations type
--- server/src/postulations/infrastructure/controller/postulation.controller.ts
+++ server/src/postulations/infrastructure/controller/postulation.controller.ts
@@ -1,5 +1,6 @@
 import { type RequestHandler } from 'express'
 import { type PostulationUseCase } from '../../aplication/postulationUseCase'
+import { type translatedResponse } from '../../../interfaces'
 
 export class PostulationController {
   constructor (private readonly postulationUseCase: PostulationUseCase) { }
@@ -18,7 +19,7 @@ export class PostulationController {
   public postController: RequestHandler = async (req, res) => {
     try {
       const postulation = await this.postulationUseCase.createPostulation(req.body)
-      return res.status(201).json(postulation)
+      return res.status(201).json(postulation[(req as any).lang as keyof translatedResponse])
     } catch (error: any) {
       return res.status(error.code).json(error[(req as any).lang as keyof Error])
     }
