4db24ad0ad0da477b646bb5a7460d1d8805ff20f
MateoGuerreroE <mateof1223@gmail.com>
2023-12-23 10:03:17 -0500

    User list as return value when editing to improve browser time loading

--- server/src/users/domain/user/user.reposiroty.ts
+++ server/src/users/domain/user/user.reposiroty.ts
@@ -1,8 +1,8 @@
 import { type UserEntity } from './user.entity'
 
 export interface UserRepository {
-  findUser: (value: string, filter: string) => Promise<UserEntity | UserEntity[] | string>
-  createUser: (user: UserEntity) => Promise<UserEntity | string>
+  findUser: (value: string, filter: string) => Promise<UserEntity | UserEntity[]>
+  createUser: (user: UserEntity) => Promise<UserEntity>
   deleteUser: (id: string, reqID?: string, total?: string) => Promise<UserEntity | string>
-  editUser: (id: string, user: UserEntity) => Promise<UserEntity | string>
+  editUser: (id: string, user: UserEntity) => Promise<UserEntity[]>
 }
