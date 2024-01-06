8756b07aa914277a570e197986fd1601617a615c
Mateo2 <mateof1223@gmail.com>
2024-01-06 15:20:53 -0500

    Fixed merge

--- server/src/postulations/infrastructure/controller/postulation.controller.ts
+++ server/src/postulations/infrastructure/controller/postulation.controller.ts
@@ -1,6 +1,5 @@
 import { type RequestHandler } from 'express'
 import { type PostulationUseCase } from '../../aplication/postulationUseCase'
-import { type translatedResponse } from '../../../interfaces'
 
 export class PostulationController {
   constructor (private readonly postulationUseCase: PostulationUseCase) { }
@@ -17,7 +16,7 @@ export class PostulationController {
   public postController: RequestHandler = async (req, res) => {
     try {
       const postulation = await this.postulationUseCase.createPostulation(req.body, req.query.user as string)
-      return res.status(201).json(postulation[(req as any).lang as keyof translatedResponse])
+      return res.status(201).json(postulation)
     } catch (error: any) {
       return res.status(error.code).json(error[(req as any).lang as keyof Error])
     }
