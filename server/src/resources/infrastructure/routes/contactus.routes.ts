e1feaf671e1fdc0303a7ef4cd188aec109a20156
juanmeyer94 <juan_meyer@hotmail.com.ar>
2024-06-10 16:24:01 -0300

    add web to calculator and airtable

--- server/src/resources/infrastructure/routes/contactus.routes.ts
+++ server/src/resources/infrastructure/routes/contactus.routes.ts
@@ -9,21 +9,23 @@ const contactUsRoute = Router()
 contactUsRoute.post('/', async (req, res): Promise<any> => {
   try {
     const newContact = req.body
-    await base('Contactos').create([
-      {
-        fields: {
-          Nombre: newContact.firstName,
-          Apellido: newContact.lastName,
-          Empresa: newContact.company,
-          '¿Qué servicio te interesa?': newContact.service,
-          Email: newContact.email,
-          Mensaje: newContact.message
-        }
+    const record = {
+      fields: {
+        Nombre: newContact.firstName,
+        Apellido: newContact.lastName,
+        Empresa: newContact.company,
+        '¿Qué servicio te interesa?': newContact.service,
+        Email: newContact.email,
+        Mensaje: newContact.message,
+        WebCliente: newContact.web
       }
-    ])
+    }
+
+    await base('Contactos').create([record])
     const response: translatedResponse = { en: 'Your information has been sent successfully', es: 'Tu informacion ha sido enviada con exito' }
     res.status(200).send(response[(req as any).lang as keyof translatedResponse])
   } catch (error: any) {
+    console.error('Error creating contact:', error) // Log de error
     const newError = new UncatchedError(error.message, 'create a contact request', 'crear una peticion de contacto')
     res.status(500).json(newError[(req as any).lang as keyof customError])
   }
